41e930f97208b037d69245aaab30347b8d6279d7
Bien <tranchanhbien@gmail.com>
2026-08-16 15:37:42 +0700

    chinh sua yeu cau tu van

--- backend/src/main/java/com/pharma/backend/enums/tuvan/TrangThaiTuVan.java
+++ backend/src/main/java/com/pharma/backend/enums/tuvan/TrangThaiTuVan.java
@@ -1,25 +1,8 @@
 package com.pharma.backend.enums.tuvan;
 
 public enum TrangThaiTuVan {
-    /**
-     * Yêu cầu mới được khách hàng gửi
-     * và đang chờ dược sĩ tư vấn.
-     */
     CHO_TIEP_NHAN,
-
-    /**
-     * Dược sĩ đã hoàn thành tư vấn.
-     */
+    DANG_TU_VAN,
     DA_TU_VAN,
-
-    /**
-     * Dược sĩ đã thử liên hệ nhưng
-     * hiện chưa thể liên lạc với khách hàng.
-     */
-    KHONG_THE_LIEN_HE,
-
-    /**
-     * Yêu cầu tư vấn đã được hủy.
-     */
-    DA_HUY
+    KHONG_THE_LIEN_HE
 }
